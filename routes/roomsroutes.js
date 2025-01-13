78b3bd28e6b19b674ff5d24eb37236fdbb9d759d
Ever Barahona <ever.barahona.mendoza@kyndryl.com>
2025-01-13 14:53:19 -0600

    removing methods

--- routes/roomsroutes.js
+++ routes/roomsroutes.js
@@ -9,19 +9,4 @@ module.exports = function(app) {
   app.route('/rooms')
     .get(rooms.list_all_rooms)
   
-  app.route('/rooms/booking')
-    .put(rooms.booking)
-  
-  app.route('/rooms/unbooking')
-    .put(rooms.unbooking)
-
-  app.route('/rooms/:roomName')
-     .get(rooms.read_a_room)
-
-  //app.route('/api/auth/:username/:password')
-  app.route('/users/auth')
-     .post(auth.authUser);
-  
-  app.route('/users')
-     .get(auth.getUsers);
 };
